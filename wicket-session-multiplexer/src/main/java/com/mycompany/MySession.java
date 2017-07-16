41492b8a1e1d63909ea56a774fc438061c73ede3
Carl-Eric Menzel <cmenzel@wicketbuch.de>
2017-07-16 23:06:41 +0200

    move things into main, -parent, and -example

---
+++ wicket-session-multiplexer/src/main/java/com/mycompany/MySession.java
@@ -0,0 +1,27 @@
+package com.mycompany;
+
+import org.apache.wicket.authroles.authentication.AuthenticatedWebSession;
+import org.apache.wicket.authroles.authorization.strategies.role.Roles;
+import org.apache.wicket.request.Request;
+
+public class MySession extends AuthenticatedWebSession {
+
+	public MySession(Request request) {
+		super(request);
+	}
+
+	@Override
+	protected boolean authenticate(String userEMail, String password) {
+		return true;
+	}
+
+	@Override
+	public Roles getRoles() {
+		Roles userRoles = new Roles();
+		if (isSignedIn()) {
+			userRoles.add(UserRoles.SIGNED_USER);
+		}
+		return userRoles;
+	}
+
+}
